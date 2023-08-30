d22f4084d582b32230df77dc09d8e2a343d174f6
jindacz <jindaz.work@outlook.com>
2023-08-30 20:39:29 +0000

    start sklearn

--- graphics/Ray Tracing in One Weekend/1/main.cc
+++ graphics/Ray Tracing in One Weekend/1/main.cc
@@ -19,21 +19,21 @@ int main() {
         for (int i = 0; i < image_width; ++i) {
 			auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
 			write_color(std::cout, pixel_color);
-			
-            // Calculate normalized values for red and green channels.
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
+
+            // // Calculate normalized values for red and green channels.
+            // auto r = double(i) / (image_width - 1);
+            // auto g = double(j) / (image_height - 1);
             
-            // Blue channel is set to 0.
-            auto b = 0;
+            // // Blue channel is set to 0.
+            // auto b = 0;
 
-            // Convert the normalized channel values to integers in the range [0, 255].
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            // // Convert the normalized channel values to integers in the range [0, 255].
+            // int ir = static_cast<int>(255.999 * r);
+            // int ig = static_cast<int>(255.999 * g);
+            // int ib = static_cast<int>(255.999 * b);
 
-            // Print the RGB values as space-separated integers.
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            // // Print the RGB values as space-separated integers.
+            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
 	std::clog << "\rDone. 			\n";
